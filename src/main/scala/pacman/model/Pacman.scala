a681fcf7047055e23383e83f6db352aced3bb520
Orlando <orlandojsjr@gmail.com>
2015-08-03 13:27:37 -0300

    pacman logic implementation and spec

--- src/main/scala/pacman/model/Pacman.scala
+++ src/main/scala/pacman/model/Pacman.scala
@@ -1,8 +1,55 @@
 package main.scala.pacman.model
 
-case class Pacman (position: Position) {
-  
-  def findFood(maze: Maze) {
-    
+import Array._
+import scala.collection.mutable.Stack
+import scala.collection.mutable.Set
+
+case class Pacman(initialPosition: Position) {
+
+  private val positionsIhaveBeen = Set[String]()
+  private var positionsStack = Stack[Position](initialPosition)
+
+  def findPathFood(maze: Maze) = {
+    while (!maze.isAFood(positionsStack.top)) {
+      this.goToNextStep(maze)
+    }
+    positionsStack
   }
+  
+  def pathToFood = positionsStack
+
+  private def goToNextStep(maze: Maze) {
+    val nextPosition = defineNextPositionStep(maze)
+    nextPosition.isDefined match {
+      case true  => positionsStack.push(nextPosition.get)
+      case false => if (positionsStack.size > 0) positionsStack.pop()
+    }
+  }
+
+  private def defineNextPositionStep(maze: Maze) = {
+    val actualPosition = positionsStack.top
+    var nextPosition = tryGetNextPositionStep(actualPosition.right, maze)
+
+    if (!nextPosition.isDefined) nextPosition = tryGetNextPositionStep(actualPosition.up, maze)
+    if (!nextPosition.isDefined) nextPosition = tryGetNextPositionStep(actualPosition.left, maze)
+    if (!nextPosition.isDefined) nextPosition = tryGetNextPositionStep(actualPosition.down, maze)
+
+    nextPosition
+  }
+
+  private def tryGetNextPositionStep(candidateNextPosition: Position, maze: Maze): Option[Position] = {
+    !iHaveBeenHere(candidateNextPosition) && maze.isAValidPosition(candidateNextPosition) match {
+      case true => {
+        pushPositionIHaveBeen(candidateNextPosition)
+        Option(candidateNextPosition)
+      }
+      case _ => None
+    }
+  }
+
+  private def pushPositionIHaveBeen(position: Position) {
+    positionsIhaveBeen.add(position.toString)
+  }
+
+  private def iHaveBeenHere(position: Position) = positionsIhaveBeen(position.toString)
 }
---
+++ src/test/scala/PacmanSpec.scala
@@ -0,0 +1,22 @@
+package test.scala
+
+import org.specs2.mutable.Specification
+import main.scala.pacman.model.Maze
+import main.scala.pacman.model.Food
+import main.scala.pacman.model.Pacman
+import main.scala.pacman.model.Position
+import scala.collection.mutable.Stack
+
+class PacmanSpec extends Specification {
+
+  "Pacman" >> {
+    "must find a food" >> {
+      val food = new Food(new Position(5, 5))
+      val maze = new Maze(food)
+      val pacman = new Pacman(new Position(5, 1))
+      val pathToFoodExpected = Stack(new Position(5, 5), new Position(5, 4), new Position(5, 3), new Position(5, 2), new Position(5, 1))
+      pacman.findPathFood(maze)
+      pacman.pathToFood.equals(pathToFoodExpected) must beTrue
+    }
+  }
+}
