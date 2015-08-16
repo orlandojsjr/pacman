e99f1135a4942e71ff47f16962ea1cd6384327bb
Orlando <orlandojsjr@gmail.com>
2015-08-16 13:22:54 -0300

    test refactoring

--- src/test/scala/ParseSpec.scala
+++ src/test/scala/ParseSpec.scala
@@ -22,22 +22,16 @@ class ParseSpec extends Specification with BeforeAll {
 
   "ParseSpec result" >> {
     "must find pacman position" >> {
-      val file = "/home/orlando/workspace/pacman/src/main/resources/maze-1"
-      val result = Parser.process(file)
       val expectedPosition = new Position(5,1)
       expectedPosition equals pacman.initialPosition
     }
 
     "must find food position" >> {
-      val file = "/home/orlando/workspace/pacman/src/main/resources/maze-1"
-      val result = Parser.process(file)
       val expectedPosition = new Position(5,5)
       expectedPosition equals maze.food.position
     }
 
     "must find maze size" >> {
-      val file = "/home/orlando/workspace/pacman/src/main/resources/maze-1"
-      val result = Parser.process(file)
       val expectedLines = 7
       val expextedColumns = 20
       expectedLines equals maze.map.length
